714a36c1f51dab354cb3ba8135cd8486300a6cb3
KangKang <kang@thinkermobile.com>
2013-09-21 10:49:02 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catfood::Application.config.secret_key_base = 'a6771fbd15fc5940955316c3ac6d45d184ca231a379fa1b38927a132e33d2d0e2d6598e33f5d275dcb55c78dc87dab67a1bc2730531d24e7fbb65bf46eecaa90'
